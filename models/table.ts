ffd81e9f038c1bdf165a4e0a66feb776f5bbdb94
Takahiro Ishii <itkh@amazon.com>
2022-08-23 09:53:43 +0900

    feat: add more comments

--- models/table.ts
+++ models/table.ts
@@ -8,37 +8,38 @@ export class ATable {
   // sk: string;
 }
 
-// 各playerはユニークなplayerID (string) を持っている * player テーブルはここでは無視する
-// 各characterはユニークなcharacterID (string) を持っている
-// 各weaponはユニークなweaponID (string) を持っている
+/*
+各playerはユニークなplayerID (string) を持っている * player テーブルはここでは無視する
+各characterはユニークなcharacterID (string) を持っている
+各weaponはユニークなweaponID (string) を持っている
 
-// characterは他にも name, characterDataID (マスターデータのID) などを持っている
-// weaponは他にも name, weaponDataId (マスターデータのID) などを持っている
+characterは他にも name, characterDataID (マスターデータのID) などを持っている
+weaponは他にも name, weaponDataId (マスターデータのID) などを持っている
+ */
 
-// characters
-
-// GET https://..../characters/playerId
+// characters ---------------------------------------------------------------------
+// TODO: GET https://..../characters/playerId
 // あるplayer１人の全characterを入手
 
-// GET https://..../characters/character/playerId/characterId
+// TODO: GET https://..../characters/character/playerId/characterId
 // あるplayerのあるcharacterを入手
 
-// POST https://..../characters/new/playerId/characterId
+// TODO: POST https://..../characters/new/playerId/characterId
 // あるplayerにあるcharacterを付与
 
-// POST https://..../characters/equip/playerId/characterId/weaponId
+// TODO: POST https://..../characters/equip/playerId/characterId/weaponId
 // あるplayerのあるcharacterにあるweaponを装着
 
-// POST https://..../characters/remove/playerId/characterId/weaponId
+// TODO: POST https://..../characters/remove/playerId/characterId/weaponId
 // あるplayerのあるcharacterについているはずのweaponを脱着
 
-// weapons
+// weapons ----------------------------------------------------------------------
 
-// GET https://..../weapons/playerId
+// TODO: GET https://..../weapons/playerId
 // あるplayer１人の全weaponを入手
 
-// GET https://..../weapons/playerId/weaponId
+// TODO: GET https://..../weapons/playerId/weaponId
 // あるplayerのあるweaponを入手
 
-// POST https://..../weapons/new/playerId/weaponId
+// TODO: POST https://..../weapons/new/playerId/weaponId
 // あるplayerにあるweaponを付与
